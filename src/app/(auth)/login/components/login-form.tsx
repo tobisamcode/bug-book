5f61aa95915ed69773ba9de6e6d4980ab692f252
tobisamcode <samueloluwatobiloba48@gmail.com>
2025-04-19 12:44:20 +0100

    auth layout setup - ongoing

---
+++ src/app/(auth)/login/components/login-form.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { loginSchema, LoginValues } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState, useTransition } from "react";
+import { useForm } from "react-hook-form";
+import { login } from "../actions";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { PasswordInput } from "@/components/password-input";
+import LoadingButton from "@/components/loading-button";
+
+export default function LoginForm() {
+  const [error, setError] = useState<string>();
+
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<LoginValues>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: LoginValues) {
+    setError(undefined);
+    startTransition(async () => {
+      const { error } = await login(values);
+      if (error) {
+        setError(error);
+        return;
+      }
+    });
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+        {error && (
+          <p className="text-destructive text-center text-sm">{error}</p>
+        )}
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input {...field} placeholder="Username" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <PasswordInput
+                  {...field}
+                  placeholder="Password"
+                  type="password"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <LoadingButton loading={isPending} type="submit" className="w-full">
+          Login
+        </LoadingButton>
+      </form>
+    </Form>
+  );
+}
