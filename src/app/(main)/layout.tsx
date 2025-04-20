c0979406b755fe1056ee2a6b90267faccc4992c3
tobisamcode <samueloluwatobiloba48@gmail.com>
2025-04-20 02:04:20 +0100

    auth completed

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import React from "react";
 import SessionProvider from "./session-provider";
 import Navbar from "./nav-bar";
+import MenuBar from "./menu-bar";
 
 export default async function Layout({
   children,
@@ -11,13 +12,18 @@ export default async function Layout({
 }) {
   const session = await validateRequest();
 
-  if (!session.user) redirect("/login");
+  if (!session.user) {
+    return redirect("/login");
+  }
 
   return (
     <SessionProvider value={session}>
       <div className="flex min-h-screen flex-col">
         <Navbar />
-        <div className="mx-auto max-w-7xl p-5">{children}</div>
+        <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
+          <MenuBar className="bg-card sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-xl px-3 py-5 shadow-sm sm:block xl:w-80" />
+          {children}
+        </div>
       </div>
     </SessionProvider>
   );
