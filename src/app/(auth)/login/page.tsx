40823caffbc190feda1604a579d6b84dfbb1c63c
tobisamcode <samueloluwatobiloba48@gmail.com>
2025-04-20 02:04:13 +0100

    auth completed

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -13,10 +13,10 @@ export default function Page() {
   return (
     <main className="flex h-screen items-center justify-center p-5">
       <div className="bg-card flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl shadow-2xl">
-        <div className="w-fit space-y-10 overflow-y-auto p-10 md:w-1/2">
+        <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
           <h1 className="text-center text-3xl font-bold">Login to bugbook</h1>
 
-          <div className="space-y-5">
+          <div className="bgblue space-y-5">
             <LoginForm />
 
             <Link
