784587a281e061d06e4e07c999d45794c8b0a25c
tobisamcode <samueloluwatobiloba48@gmail.com>
2025-04-22 19:25:51 +0100

    api and data binding

--- src/app/react-query-provider.tsx
+++ src/app/react-query-provider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { useState } from "react";
 
@@ -11,5 +12,10 @@ export default function ReactQueryProvider({
 }) {
   const [client] = useState(new QueryClient());
 
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
