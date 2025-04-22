8c156f4b2bcd6fb3141d4722f0b31de41d831a49
tobisamcode <samueloluwatobiloba48@gmail.com>
2025-04-22 10:19:59 +0100

    api and data binding

---
+++ src/lib/types.ts
@@ -0,0 +1,18 @@
+import { Prisma } from "@prisma/client";
+
+export const userDataSelect = {
+  id: true,
+  username: true,
+  displayName: true,
+  avatarUrl: true,
+};
+
+export const postDataInclude = {
+  user: {
+    select: userDataSelect,
+  },
+} satisfies Prisma.PostInclude;
+
+export type PostData = Prisma.PostGetPayload<{
+  include: typeof postDataInclude;
+}>;
