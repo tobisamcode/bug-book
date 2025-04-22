a3cef0419349b0a86a39604f19fcb9cda7c0296a
tobisamcode <samueloluwatobiloba48@gmail.com>
2025-04-22 10:19:36 +0100

    api and data binding

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,26 @@
-import Image from "next/image";
+import TrendSidebar from "@/components/trend-sidebar";
+import Post from "@/features/posts/components/post";
+import PostEditor from "@/features/posts/components/post-editor";
+import prisma from "@/lib/prisma";
+import { postDataInclude } from "@/lib/types";
 
-export default function Home() {
-  return <main>Front page</main>;
+export default async function Home() {
+  const posts = await prisma.post.findMany({
+    include: postDataInclude,
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return (
+    <main className="flex w-full min-w-0 gap-5">
+      <div className="w-full min-w-0 space-y-5">
+        <PostEditor />
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </div>
+      <TrendSidebar />
+    </main>
+  );
 }
