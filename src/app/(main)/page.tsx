4ea1c0f5bc0838c9bd9dd52f07cd3771fb32b7f4
tobisamcode <samueloluwatobiloba48@gmail.com>
2025-04-22 19:25:54 +0100

    api and data binding

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,24 +1,13 @@
 import TrendSidebar from "@/components/trend-sidebar";
-import Post from "@/features/posts/components/post";
+import ForYouFeed from "@/features/posts/components/for-you-feed";
 import PostEditor from "@/features/posts/components/post-editor";
-import prisma from "@/lib/prisma";
-import { postDataInclude } from "@/lib/types";
 
-export default async function Home() {
-  const posts = await prisma.post.findMany({
-    include: postDataInclude,
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
-
+export default function Home() {
   return (
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
-        {posts.map((post) => (
-          <Post key={post.id} post={post} />
-        ))}
+        <ForYouFeed />
       </div>
       <TrendSidebar />
     </main>
