203968e477f035c26927e88620f9ade5abf5f998
tobisamcode <samueloluwatobiloba48@gmail.com>
2025-04-22 10:20:03 +0100

    api and data binding

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,29 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+import { formatDate, formatDistanceToNowStrict } from "date-fns";
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function formatRelativeDate(from: Date) {
+  const currentDate = new Date();
+
+  if (currentDate.getTime() - from.getTime() < 1000 * 60 * 60 * 24) {
+    return formatDistanceToNowStrict(from, { addSuffix: true });
+  } else {
+    if (currentDate.getFullYear() === from.getFullYear()) {
+      return formatDate(from, "MMM dd");
+    } else {
+      return formatDate(from, "MMM dd, yyyy");
+    }
+  }
+}
+
+export function formatNumber(num: number): string {
+  return Intl.NumberFormat("en-US", {
+    notation: "compact",
+    maximumFractionDigits: 1,
+  }).format(num);
 }
