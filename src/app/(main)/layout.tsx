08d7076a2f8152f0e5ee2b82018010b591efb110
tobisamcode <samueloluwatobiloba48@gmail.com>
2025-04-22 10:19:32 +0100

    api and data binding

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -18,7 +18,7 @@ export default async function Layout({
 
   return (
     <SessionProvider value={session}>
-      <div className="flex min-h-screen flex-col">
+      <div className="bg-secondary/70 flex min-h-screen flex-col">
         <Navbar />
         <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
           <MenuBar className="bg-card sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-xl px-3 py-5 shadow-sm sm:block xl:w-80" />
