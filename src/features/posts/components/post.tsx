adb2900a383a4751f3162a04cc82c53d0c6f5d92
tobisamcode <samueloluwatobiloba48@gmail.com>
2025-04-27 23:54:01 +0100

    mutations - react query

--- src/features/posts/components/post.tsx
+++ src/features/posts/components/post.tsx
@@ -1,34 +1,49 @@
+"use client";
+
+import { useSession } from "@/app/(main)/session-provider";
 import UserAvatar from "@/components/user-avatar";
 import { PostData } from "@/lib/types";
 import { formatRelativeDate } from "@/lib/utils";
 import Link from "next/link";
+import MoreButton from "./more-button";
 
 interface PostProps {
   post: PostData;
 }
 
 export default function Post({ post }: PostProps) {
-  return (
-    <article className="bg-card space-y-3 rounded-2xl p-5 shadow-sm">
-      <div className="flex flex-wrap gap-3">
-        <Link href={`/user/${post.user.username}`}>
-          <UserAvatar avatarUrl={post.user.avatarUrl} className="h-10 w-10" />
-        </Link>
+  const { user } = useSession();
 
-        <div className="">
-          <Link
-            href={`/user/${post.user.username}`}
-            className="block font-medium hover:underline"
-          >
-            {post.user.displayName}
+  return (
+    <article className="group/post bg-card space-y-3 rounded-2xl p-5 shadow-sm">
+      <div className="flex justify-between">
+        <div className="flex flex-wrap gap-3">
+          <Link href={`/user/${post.user.username}`}>
+            <UserAvatar avatarUrl={post.user.avatarUrl} className="h-10 w-10" />
           </Link>
-          <Link
-            href={`/posts/${post.id}`}
-            className="text-muted-foreground block text-sm hover:underline"
-          >
-            {formatRelativeDate(post.createdAt)}
-          </Link>
+
+          <div className="">
+            <Link
+              href={`/user/${post.user.username}`}
+              className="block font-medium hover:underline"
+            >
+              {post.user.displayName}
+            </Link>
+            <Link
+              href={`/posts/${post.id}`}
+              className="text-muted-foreground block text-sm hover:underline"
+            >
+              {formatRelativeDate(post.createdAt)}
+            </Link>
+          </div>
         </div>
+
+        {post.user.id === user?.id && (
+          <MoreButton
+            post={post}
+            className="opacity-0 transition-opacity group-hover/post:opacity-100"
+          />
+        )}
       </div>
 
       <div className="break-words whitespace-pre-line">{post.content}</div>
